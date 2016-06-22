cdcf847ab0257076dfce1c25be5d4432d25d5d88
Dinis Vieira <dinisvieira@outlook.com>
2016-06-22 01:15:29 +0100

    added demo for windows phone

--- XamarinMemeGenerator/XamarinMemeGenerator.WinPhone/MainPage.xaml.cs
+++ XamarinMemeGenerator/XamarinMemeGenerator.WinPhone/MainPage.xaml.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
@@ -36,20 +39,33 @@ namespace XamarinMemeGenerator.WinPhone
         /// </summary>
         /// <param name="e">Event data that describes how this page was reached.
         /// This parameter is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: Prepare page for display here.
+            //Calls the Shared Portable Class Library to get a list with all available meme's.
+            ObservableCollection<string> memes = await Memes.GetMemesList();
 
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
-            Button.Click += delegate
-            {
-                var title = string.Format("{0} clicks!", count++);
-                Button.Content = title;
-            };
+            //Set the list of memes to our ComboBox and enable it
+            MemesListView.ItemsSource = memes;
+            MemesListView.IsEnabled = true;
+        }
+
+        private async void GenerateMyMemeBtn_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (MemesListView.SelectedValue == null) { return; } //make sure we have a selected value
+
+            //Calls the Shared Portable Class Library with the values of the ComboBox and TextBox's in this View.
+            //The returned value is the image in a byte array format 
+            byte[] imageBytes = await Memes.GenerateMeme(MemesListView.SelectedValue.ToString(), TopTextBox.Text, BottomTextBox.Text);
+
+            //Create Image
+            var bitmapImage = new BitmapImage();
+            var stream = new InMemoryRandomAccessStream();
+            await stream.WriteAsync(imageBytes.AsBuffer());
+            stream.Seek(0);
+            bitmapImage.SetSource(stream);
+
+            //Set image to the Image Placeholder we have on our View
+            Image.Source = bitmapImage;
         }
     }
 }
